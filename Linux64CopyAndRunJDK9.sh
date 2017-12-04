b09197d01bbb124e4685eb2d108b82b51b1c1e6b
sterg <sterg@teiemt.gr>
2017-12-04 16:08:08 +0200

    added scripts for running correctly GroovyLab on JDK 9

---
+++ Linux64CopyAndRunJDK9.sh
@@ -0,0 +1,2 @@
+cp ./GroovyLabPr/dist/*.jar .
+java -server -d64    --add-modules java.xml.bind -XX:+UseNUMA -XX:+UseParallelGC -XX:+UseCompressedOops -XX:+AggressiveOpts -Djava.library.path=.:./lib -Xss5m -Xms1500m -Xmx23500m -jar GroovyLab.jar
